fde5e5e1013abe6c352cb8ed97ef0dc25433b657
Mykhailo Dorokhov <mykhailo.dorokhov@pipedrive.com>
2024-08-27 00:20:11 +0300

    added Python support

---
+++ python/data.py
@@ -0,0 +1,222 @@
+characters = [
+    {
+        "name": "Valerie",
+        "gender": "female",
+        "age": "30",
+        "email": "valerie@nightcity.corp",
+        "auth_data": {
+            "login": "v_night",
+            "password": "majorleague"
+        }
+    },
+    {
+        "name": "Johnny Silverhand",
+        "gender": "male",
+        "age": "73",
+        "email": "johnny@samurai.web",
+        "auth_data": {
+            "login": "silverhand_rocker",
+            "password": "neverfadeaway"
+        }
+    },
+    {
+        "name": "Jackie Welles",
+        "gender": "male",
+        "age": "28",
+        "email": "jackie@valentinos.org",
+        "auth_data": {
+            "login": "jackster",
+            "password": "wellsfamily"
+        }
+    },
+    {
+        "name": "Panam Palmer",
+        "gender": "female",
+        "age": "27",
+        "email": "panam@aldecaldos.ra",
+        "auth_data": {
+            "login": "nomad_panam",
+            "password": "free_spirit"
+        }
+    },
+    {
+        "name": "Judy Alvarez",
+        "gender": "female",
+        "age": "25",
+        "email": "judy@moxes.corp",
+        "auth_data": {
+            "login": "braindance_queen",
+            "password": "deepdives"
+        }
+    },
+    {
+        "name": "River Ward",
+        "gender": "male",
+        "age": "38",
+        "email": "river@ncpd.gov",
+        "auth_data": {
+            "login": "detective_ward",
+            "password": "justice"
+        }
+    },
+    {
+        "name": "Kerry Eurodyne",
+        "gender": "male",
+        "age": "60",
+        "email": "kerry@samurai.web",
+        "auth_data": {
+            "login": "eurodyne_rock",
+            "password": "blazingguitars"
+        }
+    },
+    {
+        "name": "Rogue Amendiares",
+        "gender": "female",
+        "age": "60",
+        "email": "rogue@afterlife.corp",
+        "auth_data": {
+            "login": "queen_fixer",
+            "password": "lastmerc"
+        }
+    },
+    {
+        "name": "Takemura Goro",
+        "gender": "male",
+        "age": "45",
+        "email": "takemura@arasaka.corp",
+        "auth_data": {
+            "login": "samurai_goro",
+            "password": "honorbound"
+        }
+    },
+    {
+        "name": "Alt Cunningham",
+        "gender": "female",
+        "age": "70",
+        "email": "alt@nightcity.corp",
+        "auth_data": {
+            "login": "netrunner_alt",
+            "password": "beyondthesoul"
+        }
+    },
+    {
+        "name": "Yorinobu Arasaka",
+        "gender": "male",
+        "age": "40",
+        "email": "yorinobu@arasaka.corp",
+        "auth_data": {
+            "login": "rebel_son",
+            "password": "familysecrets"
+        }
+    },
+    {
+        "name": "Adam Smasher",
+        "gender": "male",
+        "age": "100",
+        "email": "smasher@militech.corp",
+        "auth_data": {
+            "login": "total_conversion",
+            "password": "hardcore"
+        }
+    },
+    {
+        "name": "Lizzy Wizzy",
+        "gender": "female",
+        "age": "35",
+        "email": "lizzy@nightcity.corp",
+        "auth_data": {
+            "login": "metal_star",
+            "password": "forever_young"
+        }
+    },
+    {
+        "name": "Claire Russell",
+        "gender": "female",
+        "age": "34",
+        "email": "claire@afterlife.corp",
+        "auth_data": {
+            "login": "racing_queen",
+            "password": "speeddemon"
+        }
+    },
+    {
+        "name": "Dexter DeShawn",
+        "gender": "male",
+        "age": "48",
+        "email": "dex@nightcity.corp",
+        "auth_data": {
+            "login": "bigfix_dex",
+            "password": "bigleague"
+        }
+    },
+    {
+        "name": "Evelyn Parker",
+        "gender": "female",
+        "age": "29",
+        "email": "evelyn@clouds.corp",
+        "auth_data": {
+            "login": "illusionist_eve",
+            "password": "deepweb"
+        }
+    },
+    {
+        "name": "David Martinez",
+        "gender": "male",
+        "age": "18",
+        "email": "david@nightcity.corp",
+        "auth_data": {
+            "login": "david_runner",
+            "password": "edgeofnight"
+        }
+    },
+    {
+        "name": "Lucy",
+        "gender": "female",
+        "age": "22",
+        "email": "lucy@nightcity.corp",
+        "auth_data": {
+            "login": "lucy_nethacker",
+            "password": "inthematrix"
+        }
+    },
+    {
+        "name": "Maine",
+        "gender": "male",
+        "age": "35",
+        "email": "maine@nightcity.corp",
+        "auth_data": {
+            "login": "maine_leader",
+            "password": "frontline"
+        }
+    },
+    {
+        "name": "Rebecca",
+        "gender": "female",
+        "age": "19",
+        "email": "rebecca@nightcity.corp",
+        "auth_data": {
+            "login": "rebecca_boom",
+            "password": "firepower"
+        }
+    },
+    {
+        "name": "Kiwi",
+        "gender": "female",
+        "age": "24",
+        "email": "kiwi@nightcity.corp",
+        "auth_data": {
+            "login": "kiwi_fixer",
+            "password": "gadgetgenius"
+        }
+    },
+    {
+        "name": "Falco",
+        "gender": "male",
+        "age": "40",
+        "email": "falco@fnightcity.corp",
+        "auth_data": {
+            "login": "falco_skyhigh",
+            "password": "wingman"
+        }
+    }
+]
